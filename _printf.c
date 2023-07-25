95852bccdd0349070592619f4a2951252cf557e3
Elelwani <tshikosiem@gmail.com>
2023-07-25 12:07:52 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -11,6 +11,7 @@ int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list value;
+
 	va_start(value, format);
 
 	while (*format != '\0')
@@ -27,12 +28,14 @@ int _printf(const char *format, ...)
 			else if (*format == 'c')
 			{
 				int ch = va_arg(value, int);
+
 				putchar(ch);
 				count++;
 			}
 			else if (*format == 's')
 			{
 				char *str = va_arg(value, char *);
+
 				while (*str != '\0')
 				{
 					putchar(*str);
@@ -41,15 +44,13 @@ int _printf(const char *format, ...)
 				}
 			}
 		}
-	
 		else
 		{
-			putchar(*format);
-			count++;
-			format++;
+		putchar(*format);
+		count++;
+		format++;
 		}
-	}
-	va_end(value);
-	return (count);
-
+}
+va_end(value);
+return (count);
 }
